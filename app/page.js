2158d6f870b878355681f4348d4123a4d1d2fc73
Riku Shioda <hry-23@Rikus-MacBook-Pro.local>
2025-04-16 13:47:35 +0900

    add RedButton

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
+'use client';
+
 export default function Home() {
   return (
     <div>
       <header className="header">
         <h1 className="text">ヘッダーです</h1>
+        <button className="button1" onClick={() => alert('クリックされました！')}>左ボタン</button>
       </header>
     </div>
   );
