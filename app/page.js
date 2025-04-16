7b8a8a1fcf721775fe85bd0d51c7e3dad90601ef
Riku Shioda <hry-23@Rikus-MacBook-Pro.local>
2025-04-16 13:51:09 +0900

    add BlueButton&GreenButton

--- app/page.js
+++ app/page.js
@@ -6,6 +6,8 @@ export default function Home() {
       <header className="header">
         <h1 className="text">ヘッダーです</h1>
         <button className="button1" onClick={() => alert('クリックされました！')}>左ボタン</button>
+        <button className="button2" onClick={() => alert('クリックされました！')}>中央ボタン</button>
+        <button className="button3" onClick={() => alert('クリックされました！')}>右ボタン</button>
       </header>
     </div>
   );
