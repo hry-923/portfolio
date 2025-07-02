c2fd4fdbb889c183e2ef3136dbfceef0a17047c2
Riku Shioda <hry-23@Rikus-MacBook-Pro.local>
2025-07-02 14:15:46 +0900

    add icon

--- app/page.js
+++ app/page.js
@@ -9,13 +9,21 @@ export default function Home() {
         <button className="button2" onClick={() => alert('クリックされました！')}>中央ボタン</button>
         <button className="button3" onClick={() => alert('クリックされました！')}>右ボタン</button>
       </header>
-      <body>
-      <h3>안냥</h3>
-      <h3>내 이름은 리쿠야</h3>
-      <h3>여름에 한국으로 유학 가</h3>
-      <h3>열심히 할게</h3>
-      <img src="/images/fox.jpg" alt="狐" width="300" />
-      </body>
+
+      <main className="p-4">
+        <h3>안냥</h3>
+        <h3>내 이름은 리쿠야</h3>
+        <h3>여름에 한국으로 유학 가</h3>
+        <h3>열심히 할게</h3>
+
+        <div className="w-[300px] h-[300px] rounded-full overflow-hidden border-2 border-gray-300">
+          <img
+            src="/images/fox.jpg"
+            alt="狐"
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </main>
     </div>
   );
 }
